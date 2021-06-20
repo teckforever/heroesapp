73498864fb4a560bd22214fa72237df867018653
luis moreno <teckforever@gmail.com>
2021-06-20 18:42:46 -0500

    correcion Img

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -18,7 +18,7 @@ export const HeroCard = ({
             <div className="row no-gutters">
 
                 <div className="col-md-4">
-                    <img src={`./assets/heroes/${id}.jpg`} className="card-img" alt={superhero} />
+                    <img src={`https://teckforever.github.io/heroesapp/assets/heroes/${id}.jpg`} className="card-img" alt={superhero} />
                 </div>
 
                 <div className="col-md-8">
